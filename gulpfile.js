153ab28d9624ec4c52b3bf53b837ed3652eeb288
gaspard <gaspard@gmail.com>
2015-02-25 13:18:12 +0100

    gulp: less reload on build and adding highlight.js to libs

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var less = require('gulp-less');
 gulp.task('less', function () {
   gulp.src(['./src/less/app.less', './src/less/bootstrap.less'])
     .pipe(less()) // should use paths: [ path.join(__dirname, 'src', 'includes') ]
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
 });
 
 
@@ -26,6 +27,10 @@ gulp.task('serve', ['watch'], function () {
 });
 
 gulp.task('jslib', function() {
-   gulp.src(['./bower_components/bootstrap/dist/js/bootstrap.js', './bower_components/jquery/dist/jquery.js', './bower_components/underscore/underscore.js'])
-   .pipe(gulp.dest('./build/js/lib/'));
+  gulp.src([
+    './bower_components/bootstrap/dist/js/bootstrap.js', 
+    './bower_components/jquery/dist/jquery.js', 
+    './bower_components/underscore/underscore.js',
+    './bower_components/highlight/src/highlight.js',
+  ]).pipe(gulp.dest('./build/js/lib/'));
 });
