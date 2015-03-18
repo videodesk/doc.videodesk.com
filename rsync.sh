c3fab2d1be99890838d6eebb5cd3b6eada924f8c
gaspard <gaspard@gmail.com>
2015-03-18 12:46:50 +0100

    removing rsync

--- rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# rsync doc to dev server
-# rsync . videodesk@91.121.106.24
-# WARNING, this is a git repo and 
-# dirty rsync could cause git conflicts
-rsync -a -c -z -l -vv -e ssh --delete --stats --exclude-from 'rsync_exclude.txt' "./build/" "videodesk@91.121.106.24:./www/doc.videodesk.com/build/"
